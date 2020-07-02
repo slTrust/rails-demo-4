8c1d160dc66d5249bb7cf108c7cefda98d039e6a
huangjiaxi <huangjiaxi@oak-inc.com>
2020-07-02 09:06:37 +0800

    i18n

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-
   validates_presence_of :email # 任何时候都需要 email
   # 只在创建的时候 必须给我 password_confirmation
   validates_presence_of :password_confirmation, on: [:create]
 
   # 验证邮箱格式
-  validates_format_of :email, with:/.+@.+/
+  validates_format_of :email, with:/.+@.+/ , if: :email
   # 密码长度限制
-  validates :password, length:{ minimum: 6},on:[:create]
-  # 或者这样限制长度
-  # validates_length_of :password,minimum: 6
+  validates :password, length:{ minimum: 6},on:[:create], if: :password
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,4 @@
+# 应用可用的区域设置白名单
+I18n.available_locales = [:en,'zh-CN']
+# 修改默认区域设置（默认是 : zh-CN）
+I18n.default_locale = 'zh-CN'
