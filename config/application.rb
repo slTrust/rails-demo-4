b88b886c43244dee41a2de22701aa1b3bf029683
huangjiaxi <huangjiaxi@oak-inc.com>
2020-07-02 16:51:10 +0800

    将 current_user_id 存到 session 里

--- config/application.rb
+++ config/application.rb
@@ -23,17 +23,10 @@ Dotenv::Railtie.load
 
 module RailsDemo1
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.session_store :cookie_store, key: '_my_rails_demo_session_id'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
