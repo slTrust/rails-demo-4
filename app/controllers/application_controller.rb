42433192760ca22f4fac8422a4d09fca761ec146
huangjiaxi <huangjiaxi@oak-inc.com>
2020-07-02 17:08:55 +0800

    获取当前用户的信息

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::API
+  def current_user
+    @current_user ||= User.find_by_id session[:current_user_id]
+  end
+
   def render_resources resource
+    return head 404 if resource.nil?
     if resource.errors.empty?
       render json: { resources: resource }, status: 200
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     render_resources User.create create_params
   end
 
+  def me
+    render_resources current_user
+  end
+
   def create_params
     params.permit(:email,:password,:password_confirmation)
   end
