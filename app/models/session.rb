4c092947ebf2c749c53583ffae53393a713b6cd7
huangjiaxi <huangjiaxi@oak-inc.com>
2020-07-02 15:18:22 +0800

    添加自定义校验，检查邮件是否注册

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,10 +1,18 @@
 class Session
   include ActiveModel::Model
   attr_accessor :email,:password
-
   validates :email, presence: true
+  validate :check_email , if: :email # 自定义校验
   validates :password, presence: true
+
   validates_format_of :email, with:/.+@.+/ , if: :email
   validates :password, length:{ minimum: 6}, if: :password
 
+  def check_email
+    user = User.find_by_email email
+    if user.nil?
+      # 添加一条错误规则, not_found 就是你 i18n 转译对应的 key
+      self.errors.add :email, :not_found
+    end
+  end
 end
