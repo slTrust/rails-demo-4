c97c9692641590d3f97d505ae2219a986dc9c928
huangjiaxi <huangjiaxi@oak-inc.com>
2020-07-02 10:17:07 +0800

    极限优化代码

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def create
+    render_resources User.create create_params
+  end
+
+  def create_params
+    params.permit(:email,:password,:password_confirmation)
+  end
+end
